7815469535c240f2231b30f78163a2198619d294
陈聚烺 <565027193@qq.com>
2024-02-21 19:59:38 +0800

    1

--- RubiksCube/FuncProofs.lean
+++ RubiksCube/FuncProofs.lean
@@ -58,48 +58,88 @@ section ValidityChecks
       { apply FaceTurn.R' }
       { apply FaceTurn.F' }
 
-  --todo--
-
-
-  lemma CornerTwistInvalid : CornerTwist ∉ ValidCube :=
-    by
+  lemma CornerTwistInvalid : CornerTwist ∉ ValidCube
+  := by
       simp only [CornerTwist, ValidCube]
-      intro h
-
-      -- have h2 : ∀x ∈ ({0,1,2,3,4,5,6,7} : Set (Fin 8)), (fun | 0 => 1 | _ => 0) x = 0 := Finset.sum_eq_zero_iff.mp h
+      -- intro h
+      simp only [Fin.zero_eta, imp_false, Finset.mem_singleton, Finset.mem_insert, zero_ne_one,
+        false_or, Set.mem_setOf_eq, map_one, forall_true_left, Pi.zero_apply, Finset.sum_const_zero,
+        and_true, true_and]
+      --提示有了，但是还是报错了？
+      -- exact
+      --   (bne_iff_ne
+      --         (Finset.sum {0, 1, 2, 3, 4, 5, 6, 7} fun x ↦
+      --           match x with
+      --           | { val := 0, isLt := .(Nat.mod_lt 0 Fin.instOfNatFin.proof_1) } => 1
+      --           | x => 0)
+      --         0).mp
+      --     rfl
       sorry
+      -- have h2 : ∀x ∈ ({0,1,2,3,4,5,6,7} : Set (Fin 8)), (fun | 0 => 1 | _ => 0) x = 0 := Finset.sum_eq_zero_iff.mp h
 
   lemma EdgeFlipInvalid : EdgeFlip ∉ ValidCube :=
     by
       simp [EdgeFlip, ValidCube]
+      -- 又有提示，但是证明失败：
+      -- exact
+      --   (bne_iff_ne
+      --         (Finset.sum {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11} fun x ↦
+      --           match x with
+      --           | { val := 0, isLt := .(Nat.mod_lt 0 Fin.instOfNatFin.proof_1) } => 1
+      --           | x => 0)
+      --         0).mp
+      --     rfl
       sorry
 
 end ValidityChecks
 
+--todo--
 
-/- This theorem shows that the set of valid cube states as defined in terms of permutations and orientations of the pieces contains all positions reachable with standard Rubik's cube moves. Further showing that these two sets are in fact the same is equivalent to providing a solution algorithm for any valid cube state. I do not have a proof that the solution algorithm in `SolutionAlgorithm.lean` will solve any valid cube, but I am confident that this is the case (assuming no bugs in my concretely defined setup moves). -/
--- theorem reachable_valid : ∀x : RubiksSuperType, Reachable x → x ∈ ValidCube := by
---   intro x hrx
---   induction hrx with
---   | Solved =>
---       simp [Solved, ValidCube]
---       apply And.intro
---       { apply Eq.refl }
---       { apply And.intro
---         { apply Eq.refl }
---         { apply Eq.refl } }
---   | FT c hc =>
---       cases hc with
---       | _ =>
---           simp [ValidCube]
---           apply And.intro
---           { apply Eq.refl }
---           { apply And.intro
---             { apply Eq.refl }
---             { apply Eq.refl } }
---   | mul x y hrx hry a_ih a_ih_1 =>
---       apply RubiksGroup.mul_mem'
---       all_goals assumption
+/- This theorem shows that the set of valid cube states as defined in terms of permutations and orientations of
+the pieces contains all positions reachable with standard Rubik's cube moves. Further showing that these
+two sets are in fact the same is equivalent to providing a solution algorithm for any valid cube state.
+I do not have a proof that the solution algorithm in `SolutionAlgorithm.lean` will solve any valid cube,
+but I am confident that this is the case (assuming no bugs in my concretely defined setup moves). -/
+
+-- 魔方第二基本定理的左推右部分：
+-- 所以说，魔方第二基本定理的右推左部分还没有给出：
+theorem valid_reachable
+:∀x : RubiksSuperType, x ∈ ValidCube → Reachable x
+:= by sorry
+
+theorem reachable_valid
+: ∀x : RubiksSuperType, Reachable x → x ∈ ValidCube
+:= by
+  intro x hrx
+  induction hrx with
+  | Solved =>
+      simp [Solved, ValidCube]
+      apply And.intro
+      { apply Eq.refl }
+      { apply And.intro
+        { apply Eq.refl }
+        { apply Eq.refl } }
+  | FT c hc =>
+      cases hc with
+      | _ =>
+          simp [ValidCube]
+          apply And.intro
+          { apply Eq.refl }
+          { apply And.intro
+            { apply Eq.refl }
+            { apply Eq.refl } }
+  | mul x y hrx hry a_ih a_ih_1 =>
+      apply RubiksGroup.mul_mem'
+      all_goals assumption
+
+theorem RubikCubeSecondBasicRule
+: ∀x : RubiksSuperType, Reachable x ↔ x ∈ ValidCube
+:= by
+  intro h
+  constructor
+  · exact reachable_valid h
+  · exact valid_reachable h
+  done
 
 -- -- instance {n : ℕ} {α : Type*} [DecidableEq α] : DecidableEq (Fin n → α) :=
 -- --   fun f g => Fintype.decidablePiFintype f g
@@ -134,4 +174,4 @@ end ValidityChecks
 --   { sorry }
 --   { sorry }
 
--- #check Equiv.Perm.permGroup.mul_assoc
+#check Equiv.Perm.permGroup.mul_assoc
