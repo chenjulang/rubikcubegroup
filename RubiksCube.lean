d53d2b0d41277c47dd47c0955558c19794c9ff3e
陈聚烺 <565027193@qq.com>
2024-03-02 18:01:35 +0800

    1

--- RubiksCube.lean
+++ RubiksCube.lean
@@ -1,4 +1,5 @@
 -- This module serves as the root of the `RubiksCube` library.
 -- Import modules here that should be built as part of the library.
 import «RubiksCube».RubiksCubeFunc
+import «RubiksCube».FuncProofs
 import «RubiksCube».SolutionAlgorithm
