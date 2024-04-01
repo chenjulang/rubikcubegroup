77fcc701e892489df0b6e917864e9fd588315ae8
陈聚烺 <565027193@qq.com>
2024-04-02 03:05:37 +0800

    1

--- RubiksCube/Test.lean
+++ RubiksCube/Test.lean
@@ -14,22 +14,22 @@ def reshow_OddToEven := G5Perm⁻¹
 
 
 -- 4个分别还原操作：
-def reshow_Corner_Permute:= (conj R2 (VarR G4Perm_List)⁻¹)
+def solve_Corner_Permute:= ((conj R2 (VarR G4Perm_List)⁻¹)
 * (VarL G4Perm_List)
 * (conj (D*L'*D'*F2*L) (VarL G4Perm_List))
-* (conj L2 (G4Perm)⁻¹)
+* (conj L2 (G4Perm)⁻¹))⁻¹
 
-def reshow_Edge_Permute := (conj (U'*L2*U*B') (VarL G3Perm_List))
+def solve_Edge_Permute := ((conj (U'*L2*U*B') (VarL G3Perm_List))
 * (conj F (G3Perm)⁻¹)
 * (conj F2 (VarR G3Perm_List)⁻¹)
-* (conj (D'*L2) G3Perm)
+* (conj (D'*L2) G3Perm))⁻¹
 
-def reshow_Corner_Orient:=
-((conj D G1Perm)
+def solve_Corner_Orient:=
+(conj D G1Perm)
 * (conj (U'*D) G1Perm)
 * (conj (U*D') G1Perm)
 * (conj D' G1Perm)
-* (conj D2 G1Perm) * (conj D2 G1Perm))⁻¹
+* (conj D2 G1Perm) * (conj D2 G1Perm)
 
 def reshow_Edge_Orient:= 1
 
@@ -37,29 +37,42 @@ def reshow_Edge_Orient:= 1
 -- 检验：
 --1:
 -- 一步步检查问题：
-#eval (conj R2 (VarR G4Perm_List)⁻¹) --ok
-#eval (VarL G4Perm_List) --ok
-#eval (conj (D*L'*D'*F2*L) (VarL G4Perm_List)) -- ok
-#eval (conj L2 (G4Perm)⁻¹) --ok
-#eval (reshow_Corner_Permute * reshow_OddToEven ).1.permute = statusA.1.permute -- true
+-- #eval (conj R2 (VarR G4Perm_List)⁻¹) --ok
+-- #eval (VarL G4Perm_List) --ok
+-- #eval (conj (D*L'*D'*F2*L) (VarL G4Perm_List)) -- ok
+-- #eval (conj L2 (G4Perm)⁻¹) --ok
+-- #eval (reshow_Corner_Permute * reshow_OddToEven ).1.permute = statusA.1.permute -- true
 
 --2:
-#eval (reshow_Edge_Permute * reshow_OddToEven ).2.permute = statusA.2.permute -- true
+-- #eval (reshow_Edge_Permute * reshow_OddToEven ).2.permute = statusA.2.permute -- true
 
 --3:
 -- 一步步检查问题：
-#eval (conj D G1Perm) --ok
-#eval (conj (U'*D) G1Perm) --ok
-#eval (conj (U*D') G1Perm) --ok
-#eval (conj D' G1Perm) --ok
-#eval  (conj D2 G1Perm) * (conj D2 G1Perm)
-#eval reshow_Corner_Orient.1.orient = (Corner_Absolute_Orient statusA.1) -- true
+-- #eval (conj D G1Perm) --ok
+-- #eval (conj (U'*D) G1Perm) --ok
+-- #eval (conj (U*D') G1Perm) --ok
+-- #eval (conj D' G1Perm) --ok
+-- #eval  (conj D2 G1Perm) * (conj D2 G1Perm)
+-- #eval reshow_Corner_Orient.1.orient = (Corner_Absolute_Orient statusA.1) -- true
 
 --4:
-#eval (reshow_Corner_Orient * reshow_OddToEven).2.orient = statusA.2.orient -- true
+-- #eval (reshow_Corner_Orient * reshow_OddToEven).2.orient = statusA.2.orient -- true
 
 
-#eval (reshow_Corner_Permute*reshow_Edge_Permute*reshow_Corner_Orient) * reshow_OddToEven = statusA
-#eval statusA
--- 问题1:顺序一定要这样吗？？？？？
--- 问题2:程序里顺序是先方向数后顺序的，是否是错的？
+-- 还原方向数：
+#eval (statusA * solve_Corner_Orient).1.orient = 0
+-- 还原角块位置：
+#eval (statusA*OddToEven * solve_Corner_Permute).1.permute = 1
+-- 还原棱块位置：
+#eval (statusA*OddToEven * solve_Edge_Permute).2.permute = 1
+
+-- 还原魔方：
+-- 1.先方向，后位置(先角后棱)
+#eval statusA * (OddToEven * solve_Corner_Orient * solve_Corner_Permute * solve_Edge_Permute)
+  = Solved -- true
+-- 1.先方向，后位置(先棱后角)
+#eval statusA * (OddToEven * solve_Corner_Orient * solve_Edge_Permute * solve_Corner_Permute)
+  = Solved -- true
+
+#eval statusA * (solve_Corner_Orient * OddToEven * solve_Edge_Permute * solve_Corner_Permute)
+  = Solved -- true
